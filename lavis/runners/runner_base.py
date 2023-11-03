d13f9657614a6be7553c850b7f95b4c31832eeef
HustQBW <bowenqu@stu.pku.edu.cn>
2023-11-03 15:45:02 +0800

    解决单卡问题

--- lavis/runners/runner_base.py
+++ lavis/runners/runner_base.py
@@ -21,6 +21,7 @@ from lavis.common.dist_utils import (
     get_world_size,
     is_main_process,
     main_process,
+    is_dist_avail_and_initialized,
 )
 from lavis.common.registry import registry
 from lavis.common.utils import is_url
@@ -434,7 +435,8 @@ class RunnerBase:
             if self.evaluate_only:
                 break
 
-            dist.barrier()
+            if is_dist_avail_and_initialized():
+                dist.barrier()
 
         # testing phase
         test_epoch = "best" if len(self.valid_splits) > 0 else cur_epoch
