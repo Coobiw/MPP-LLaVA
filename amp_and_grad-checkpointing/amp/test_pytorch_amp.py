6f6d32a3dc8e7b0d1c2ae3c35fd3f5b4943b362b
HustQBW <bowenqu@stu.pku.edu.cn>
2023-12-08 15:59:07 +0800

    加入amp和grad-checkpoint的tutorials

---
+++ amp_and_grad-checkpointing/amp/test_pytorch_amp.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.cuda.amp import autocast, GradScaler
+
+import time
+import os
+
+from eva_vit import create_eva_vit_g
+
+# 定义训练函数
+def train(epochs=1):
+    torch.cuda.reset_peak_memory_stats()
+
+    # 定义模型、优化器
+    model = create_eva_vit_g(img_size=224,drop_path_rate=0.,use_checkpoint=False,precision="fp32").cuda()
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    scaler = GradScaler()
+
+    # 开始训练并记录时间和显存
+    start_time = time.time()
+    for epoch in range(epochs):
+        optimizer.zero_grad()
+        inputs = torch.randn(16,3,224,224).cuda()
+        with autocast():
+            outputs = model(inputs)
+        loss = outputs.sum()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+    end_time = time.time()
+
+    # 计算并返回时间和显存使用量
+    duration = end_time - start_time
+    memory = torch.cuda.max_memory_allocated()
+    return duration, memory
+
+
+duration, memory = train()
+print(f"Opt Level: Pytorch(Equal to O1), Duration: {duration} seconds, Max Memory: {memory} bytes")
